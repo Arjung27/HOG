e025e86433ef0ed8f7083f74c4ce03f69a4c1d41
azshue <azure.Shu1997@gmail.com>
2020-09-10 00:40:00 -0400

    train scripts

--- train.py
+++ train.py
@@ -22,18 +22,19 @@ import torchvision.datasets as datasets
 
 import hog
 
-model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
-    and callable(models.__dict__[name]))
+# model_names = sorted(name for name in models.__dict__
+#     if name.islower() and not name.startswith("__")
+#     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                        ' | '.join(model_names) +
-                        ' (default: resnet18)')
+# parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+#                     choices=model_names,
+#                     help='model architecture: ' +
+#                         ' | '.join(model_names) +
+#                         ' (default: resnet18)')
+parser.add_argument('-a', '--arch', type=str, default='vgg11_H1')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
---
+++ train.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#SBATCH --partition scavenger
+
+#SBATCH --gres=gpu:4
+
+#SBATCH --ntasks=16
+
+#SBATCH --mem=128G
+
+#SBATCH --account=scavenger
+
+#SBATCH --qos=scavenger
+
+#SBATCH --time=72:00:00
+
+#SBATCH --output=cls-test.out
+
+#SBATCH --job-name=cls_test
+
+source /cmlscratch/${USER}/anaconda3/etc/profile.d/conda.sh
+conda activate python37
+
+export PYTHONPATH=./
+#eval "$(conda shell.bash hook)"
+#conda activate pt140  # pytorch 1.4.0 env
+PYTHON=python
+
+now=$(date +"%Y%m%d_%H%M%S")
+
+$PYTHON -u ${exp_dir}/train.py /fs/cml-datasets/ImageNet/ILSVRC2012 \
+  --arch vgg11_H1 -j 16 --lr 0.01 \
+  --world-size 1 --rank 0 --dist-url tcp://localhost:17456 --multiprocessing-distributed \
+  --hog_ppc (3, 3) --hog_cpb (1, 1) --stacked_dims \
+  2>&1 | tee ${model_dir}/train-$now.log
+
